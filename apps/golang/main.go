23446c058b5e8434e7aae4d8c8c9a426916c30b2
Taylor Silva <dev@taydev.net>
2025-08-11 16:47:10 -0400

    update go app
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- apps/golang/main.go
+++ apps/golang/main.go
@@ -1,11 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
-func AddSix(i int) int {
+func AddSix(i float64) float64 {
 	return i + 6
 }
 
 func main() {
-	fmt.Println("Hello world")
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a number as an argument")
+	}
+
+	num, err := strconv.ParseFloat(os.Args[1], 64)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	result := AddSix(num)
+	fmt.Println(result)
 }
--- apps/golang/main_test.go
+++ apps/golang/main_test.go
@@ -8,4 +8,7 @@ func TestAddSix(t *testing.T) {
 	if AddSix(2) != 8 {
 		t.Error("Expected 2 + 6 to equal 8")
 	}
+	if AddSix(3.3) != 9.3 {
+		t.Error("Expected 3.3 + 6 to equal 9.3")
+	}
 }
